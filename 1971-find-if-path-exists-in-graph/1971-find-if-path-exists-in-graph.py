5cd935f0dfd4c8033c62b96600b8a1ca4db3bf9a
Tsehayneh Getaneh <93071822+TsehaynehGetaneh@users.noreply.github.com>
2023-05-17 19:27:57 +0300

    Time: 1902 ms (70.75%), Space: 104.9 MB (87.48%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,20 +1,32 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         parents = [i for i in range(n)]
+        rank = [1] *n
         
         
         def representative(x):
             if parents[x] == x:
                 return x
             
-            return representative(parents[x])
+            # path compression
+            parents[x] = representative(parents[x])
+            
+            return parents[x]
         
         def union(x,y):
             parx = representative(x)
             pary = representative(y)
             
             if parx != pary:
-                parents[pary] = parx
+                if rank[parx] < rank[pary]:
+                    parents[parx] = pary
+                
+                elif rank[parx] > rank[pary]:
+                    parents[pary] = parx
+                    
+                else:
+                    parents[pary] = parx
+                    rank[parx] += 1
                 
                 
         for a,b in edges:
