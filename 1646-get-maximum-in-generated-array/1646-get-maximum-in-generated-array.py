2c8efd8bf6f3fb7c835c74e2232d3be43e0b31c7
Tsehayneh Getaneh <93071822+TsehaynehGetaneh@users.noreply.github.com>
2023-05-28 22:55:45 +0300

    Time: 56 ms (5.50%), Space: 16.3 MB (10.13%) - LeetHub

---
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.py
@@ -0,0 +1,29 @@
+class Solution:
+    def getMaximumGenerated(self, n: int) -> int:
+        nums = [0,1]
+        if n == 0:
+            return 0
+        
+        if n <= 1:
+            return 1
+        
+        i = 1
+        while i <= n:
+            idx1 = 2*i
+            idx2 = 2*i + 1
+            
+            val_idx1 = nums[i]
+            nums.append(val_idx1)
+            
+            if len(nums) == n+1:
+                return max(nums)
+            
+            val_idx2 = nums[i] + nums[i+1]
+            nums.append(val_idx2)
+            
+            if len(nums) == n+1:
+                return max(nums)
+            
+            i += 1
+            
+        
