cabc7dda30ef3efcebd6fb5395197c13891e2595
Tsehayneh Getaneh <93071822+TsehaynehGetaneh@users.noreply.github.com>
2023-07-19 08:25:59 +0300

    Time: 337 ms (57.33%), Space: 20.4 MB (35.66%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -1,9 +1,12 @@
 class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
-        
+        memo = {}
         def dp(num,steps):
             if num == 1:
                 return steps
+            
+            if num in memo:
+                return steps + memo[num]
             
             if num%2 == 0:
                 return dp(num//2,steps+1)
@@ -17,7 +20,8 @@ class Solution:
         for i in range(lo,hi+1):
             count = dp(i,0)
             res.append((count,i))
+            memo[i] = count
             
         res.sort()
-        
+        print(memo)
         return res[k-1][1]
