c4cd7ed6c8d5f9907a05ec8ec02fd2cfa647e9d9
Tsehayneh Getaneh <93071822+TsehaynehGetaneh@users.noreply.github.com>
2023-09-24 17:21:21 +0300

    Time: 642 ms (21.22%), Space: 167.3 MB (9.68%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,10 +1,19 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = [0] * (amount+1)
-        dp[0] = 1
+        cache = {}
         
-        for coin in coins:
-            for i in range(coin,amount+1):
-                dp[i] += dp[i-coin]
-                
-        return dp[amount]
+        def dp(i,total):
+            if total == amount:
+                return 1
+            
+            if total > amount or i == len(coins):
+                return 0
+            
+            if (i,total) in cache:
+                return cache[(i,total)]
+            
+            cache[(i,total)] = dp(i,total+coins[i]) + dp(i+1,total)
+            
+            return cache[(i,total)]
+        
+        return dp(0,0)
