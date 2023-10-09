249dce10e8a6e592453ba1b5281c29eddfc13233
Tsehayneh Getaneh <93071822+TsehaynehGetaneh@users.noreply.github.com>
2023-10-09 11:59:31 +0300

    Time: 28 ms (97.40%), Space: 16.2 MB (82.07%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,18 +1,43 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        for i in range(len(haystack)):
-            j = 0
-            k = i
-            while j < len(needle) and k < len(haystack):
-                if haystack[k] != needle[j]:
-                    break
-                    
-                else:
-                    j += 1
-                    k += 1
-                    
-            if j == len(needle):
-                return i
-            
-        return - 1
+        # hash the pattern and substring
+        pattern_hash = 0
+        match_hash = 0
+        power = 0
+        for i in range(len(needle)-1,-1,-1):
+            if i < len(needle):
+                hash_val_1 = ord(needle[i]) - ord("a") + 1
+                pattern_hash += (hash_val_1 * 27**power)
+                
+            if i < len(haystack):
+                hash_val_2 = ord(haystack[i]) - ord("a") + 1
+                match_hash += (hash_val_2 * 27**power)
+                
+            power += 1
+       
+        # check first match
+        if match_hash == pattern_hash:
+            return 0
         
+        for i in range(len(haystack)):
+            # poll first operation
+            hash_val_first = ord(haystack[i]) - ord("a") + 1
+            poll_first = match_hash - (hash_val_first * 27**(len(needle)-1))
+            match_hash = poll_first
+            
+            # add last operation
+            if i+len(needle) < len(haystack):
+                hash_val_last = ord(haystack[i+ len(needle)]) - ord("a") + 1
+                add_last = match_hash*27 + (hash_val_last)
+                match_hash = add_last
+            
+            if match_hash == pattern_hash:
+                print(i,pattern_hash, match_hash)
+                return i+1
+            
+        return -1
+            
+            
+            
+            
+                
