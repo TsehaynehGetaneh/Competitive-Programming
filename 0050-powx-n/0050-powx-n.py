84d8ef280dd1d31135bf24482218398389bc4ff5
Tsehayneh Getaneh <93071822+TsehaynehGetaneh@users.noreply.github.com>
2023-10-21 00:24:43 +0300

    Time: 35 ms (75.72%), Space: 16.2 MB (83.20%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,4 +1,18 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x**n
+        def recursive(x,n):
+            if x == 0:
+                return 0
+            if n == 0:
+                return 1
+            
+            ans = recursive(x,n//2)
+            ans = ans * ans
+            
+            return x*ans if n%2 != 0 else ans 
+            
+        res = recursive(x,abs(n))
+        
+        
+        return res if n >= 0 else 1/res
         
