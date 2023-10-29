a24d7185b27999b6f9d4a364658d68cb7f149f18
Tsehayneh Getaneh <93071822+TsehaynehGetaneh@users.noreply.github.com>
2023-10-29 22:55:34 +0300

    Time: 1591 ms (69.95%), Space: 104.8 MB (98.80%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,6 +1,7 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         root = [i for i in range(n)]
+        rank = [1 for i in range(n)]
         
         def find(x):
             if x == root[x]:
@@ -12,11 +13,19 @@ class Solution:
             return root[x]
         
         def union(x,y):
+            nonlocal rank
             rootX = find(x)
             rootY = find(y)
             
             if rootX != rootY:
-                root[rootX] = rootY
+                if rank[rootX] < rank[rootY]:
+                    root[rootX] = rootY
+                elif rank[rootX] > rank[rootY]:
+                    root[rootY] = rootX
+                    
+                else:
+                    root[rootY] = rootX
+                    rank[rootX] += 1
                 
         for a,b in edges:
             union(a,b)
